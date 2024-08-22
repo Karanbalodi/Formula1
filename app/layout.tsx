a6a743c0d807f5470a41c583c230bfa890f43e8c
Karan Balodi <karan.balodi@moonshinetechnology.com>
2024-08-23 03:55:22 +0530

    FEAT: analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { defaults } from "chart.js";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 export const metadata: Metadata = {
   title: "F1 - race analytics application",
-  description: "Provide detailed analysis about F1 race, lap summary player and lap speed analysis",
+  description:
+    "Provide detailed analysis about F1 race, lap summary player and lap speed analysis",
 };
 
 export default function RootLayout({
@@ -15,7 +19,11 @@ export default function RootLayout({
   defaults.font.family = "f-bold";
   return (
     <html lang="en">
-      <body className="px-8">{children}</body>
+      <body className="px-8">
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
